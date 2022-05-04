29466d299f8e52f42595dd8b9a35d3ca9e03cc59
Nathan Boisvert <nathan.boisvert@mymail.champlain.edu>
2022-05-04 15:45:43 -0400

    Modified Combat to make use of Confidence values and used techniques from GDC talk.

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public int team;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Unit _unit = collision.gameObject.GetComponent<Unit>();
+        if (_unit)
+        {
+            if(_unit.GetTeam() != team)
+            {
+                //Debug.Log("Ow");
+                _unit.DamageUnit(10);
+                Destroy(this.gameObject);
+            }
+        }
+        Destroy(this.gameObject, 5.0f);
+    }
+}
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -5,6 +5,7 @@ using UnityEngine.AI;
 
 public enum UnitType { NONE, HUNTER, GUARD, CAPTAIN, SUPPORT, TANK, SNIPER }
 
+[SerializeField]
 public struct ForwardLine
 {
     public Vector3 combatDir;
@@ -34,6 +35,8 @@ public class Unit : MonoBehaviour
     [SerializeField]
     private bool moving;
     [SerializeField]
+    private bool engaged;
+    [SerializeField]
     private float aggro;
     [SerializeField]
     private ForwardLine forwardLine;
@@ -41,20 +44,27 @@ public class Unit : MonoBehaviour
     private float averageConfidence;
     [SerializeField]
     private int unitCount;
+    [SerializeField]
+    private int health;
 
     private Confidence confidence;
 
     public UnitManager manager;
+
+    public GameObject projectile;
 
     public void SetUnitType(UnitType _type) { type = _type; }
 
     public void SetTeam(int _team) { team = _team; }
     public int GetTeam() { return team; }
 
+    public void DamageUnit(int amount) { health -= amount; }
+
     private void Start()
     {
         manager = GameObject.FindGameObjectWithTag("GameController").GetComponent<UnitManager>();
         confidence = GetComponent<Confidence>();
+        health = 100;
         switch (team) 
         {
             case 1:
@@ -78,33 +88,21 @@ public class Unit : MonoBehaviour
             default:
                 break;
         }
+
         currentTerrain = CheckTerrain();
         if(currentTerrain)
             SetTerrainConfidence(currentTerrain);
+
+        averageConfidence += confidence.GetConfidence();
+        unitCount = manager.numUnits;
+
         if (type == UnitType.CAPTAIN)
         {
-            unitCount = manager.numUnits;
-            SetLeader(unitCount);
+            leader = true;
         }
         else
         {
-            foreach (GameObject unit in GameObject.FindGameObjectsWithTag("Unit"))
-            {
-                if (unit)
-                {
-                    Unit _unit = unit.GetComponent<Unit>();
-                    if (_unit)
-                    {
-                        if (_unit.GetTeam() == team)
-                        {
-                            if (_unit.type == UnitType.CAPTAIN)
-                            {
-                                leadUnit = unit;
-                            }
-                        }
-                    }
-                }
-            }
+            leader = false;
         }
     }
 
@@ -127,6 +125,19 @@ public class Unit : MonoBehaviour
 
     private void Update()
     {
+        if(health <= 0)
+        {
+            Die();
+        }
+        if (leader && currentTarget == null)
+        {
+            RemoveLeader(this.gameObject);
+        }
+        //else if (!leader && leadUnit == null || !leader && dest == null)
+        //{
+        //    engaged = false;
+        //    moving = false;
+        //}
         Transform newTerrian = CheckTerrain();
         if (newTerrian != currentTerrain)
         {
@@ -139,46 +150,39 @@ public class Unit : MonoBehaviour
         }
         else
         {
-            //Debug.Log("Same");
-            if (leader)
+            if (!moving)
             {
-                if (!moving)
-                {
-                    StartCoroutine(Wander());
-                    moving = true;
-                }
-                foreach (Transform t in transform.parent.GetComponentInChildren<Transform>())
-                {
-                    averageConfidence += t.gameObject.GetComponent<Confidence>().GetConfidence();
-                }
-                averageConfidence /= manager.numUnits;
+                StartCoroutine(Wander());
+                moving = true;
             }
-            else
+            int layerID = 9;
+            int layerMask = 1 << layerID;
+            Collider[] colliders = Physics.OverlapSphere(transform.position, aggro, layerMask);
+            foreach (Collider collider in colliders)
             {
-                switch (type)
+                if (collider.gameObject != this.gameObject)
                 {
-                    case UnitType.HUNTER:
-                        dest = leadUnit.transform.GetChild(0);
-                        break;
-                    case UnitType.GUARD:
-                        dest = leadUnit.transform.GetChild(1);
-                        break;
-                    case UnitType.SUPPORT:
-                        dest = leadUnit.transform.GetChild(2);
-                        break;
-                    case UnitType.SNIPER:
-                        dest = leadUnit.transform.GetChild(3);
-                        break;
-                    case UnitType.TANK:
-                        dest = leadUnit.transform.GetChild(4);
-                        break;
-                    default:
-                        break;
+                    if(collider.gameObject.GetComponent<Unit>().GetTeam() == team)
+                    {
+                        averageConfidence += collider.gameObject.GetComponent<Confidence>().GetConfidence();
+                    }
                 }
-                SetDestination(dest.position);
             }
+            averageConfidence /= unitCount;
         }
-        CheckEnemies();
+        if (!engaged)
+        {
+            CheckEnemies();
+        }
+    }
+
+    private void Die()
+    {
+        if (leader)
+        {
+            RemoveLeader(this.gameObject);
+        }
+        Destroy(this.gameObject);
     }
 
     private void CheckEnemies()
@@ -197,15 +201,10 @@ public class Unit : MonoBehaviour
                             //Debug.Log("Target Found");
                             currentTarget = unit;
                             StopAllCoroutines();
+                            SetLeader(this.gameObject);
                             if (leader) 
                             {
-                                transform.LookAt(currentTarget.GetComponent<Unit>().leadUnit.transform);
-                                CalculateCombatDirection();
-                                forwardLine.width = 6;
-                                //forwardLine.neutralArea = new Rect(transform.position.x, transform.position.z - forwardLine.width,
-                                //    forwardLine.width, forwardLine.combatDir.magnitude);
-                                AdjustCondfidence();
-                                DetermineAction();
+                                StartCoroutine(Combat());
                             }
                         }
                     }
@@ -214,9 +213,25 @@ public class Unit : MonoBehaviour
         }
     }
 
+    private IEnumerator Combat()
+    {
+        while (currentTarget)
+        {
+            transform.LookAt(currentTarget.transform);
+            CalculateCombatDirection();
+            forwardLine.width = 6;
+            //forwardLine.neutralArea = new Rect(transform.position.x, transform.position.z - forwardLine.width,
+            //    forwardLine.width, forwardLine.combatDir.magnitude);
+            AdjustCondfidence();
+            Formation(this.gameObject);
+            DetermineAction();
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
     private void AdjustCondfidence()
     {
-        int enemyUnits = currentTarget.GetComponent<Unit>().leadUnit.GetComponent<Unit>().unitCount;
+        int enemyUnits = currentTarget.GetComponent<Unit>().unitCount;
         int diff = unitCount - enemyUnits;
         switch (diff)
         {
@@ -236,9 +251,30 @@ public class Unit : MonoBehaviour
         }
     }
 
+    private void Formation(GameObject lead)
+    {
+        int currUnit = 0;
+        foreach (GameObject unit in GameObject.FindGameObjectsWithTag("Unit"))
+        {
+            if (unit && unit != lead)
+            {
+                Unit _unit = unit.GetComponent<Unit>();
+                if (_unit)
+                {
+                    if (_unit.GetTeam() == team)
+                    {
+                        _unit.dest = lead.transform.GetChild(currUnit).transform;
+                        _unit.SetDestination(_unit.dest.position);
+                        currUnit++;
+                    }
+                }
+            }
+        }
+    }
+
     private void DetermineAction()
     {
-        float enemyConfidence = currentTarget.GetComponent<Unit>().leadUnit.gameObject.GetComponent<Unit>().averageConfidence;
+        float enemyConfidence = currentTarget.GetComponent<Unit>().averageConfidence;
         if(averageConfidence < (enemyConfidence - enemyConfidence * 0.5))
         {
             Retreat();
@@ -255,17 +291,47 @@ public class Unit : MonoBehaviour
 
     private void EngageCombat()
     {
-        Debug.Log("Combat");
+        //Debug.Log("Combat");
+        foreach (GameObject unit in GameObject.FindGameObjectsWithTag("Unit"))
+        {
+            if (unit)
+            {
+                Unit _unit = unit.GetComponent<Unit>();
+                if (_unit)
+                {
+                    if (_unit.GetTeam() == team)
+                    {
+                        GameObject proj;
+                        float forceMultiplier = 20;
+                        if (forceMultiplier > 0)
+                        {
+                            proj = Instantiate(projectile, _unit.gameObject.transform.position + new Vector3(0, 1, 0), unit.transform.rotation);
+                            proj.GetComponent<Projectile>().team = team;
+                            //proj.transform.LookAt(currentTarget.transform);
+                            Vector3 fireDirection = currentTarget.transform.position - unit.transform.position;
+                            proj.GetComponent<Rigidbody>().velocity = fireDirection.normalized * forceMultiplier;
+                            //proj.GetComponent<Rigidbody>().velocity.y = 0;
+                            //proj.GetComponent<Rigidbody>().AddRelativeForce(fireDirection * forceMultiplier, ForceMode.Acceleration);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     private void Retreat()
     {
-        Debug.Log("Retreat");
+        //Debug.Log("Retreat");
+        Vector3 retreatDest = transform.position - new Vector3(1, 0, 1);
+        SetDestination(retreatDest);
+        EngageCombat();
     }
 
     private void PressAttack()
     {
-        Debug.Log("Advantage");
+        //Debug.Log("Advantage");
+        SetDestination(currentTarget.transform.position);
+        EngageCombat();
     }
 
     private void CalculateCombatDirection()
@@ -286,16 +352,68 @@ public class Unit : MonoBehaviour
         Debug.DrawLine(averagePos, averageEnemyPos);
     }
 
-    private void SetLeader(int unitNum)
+    private void SetLeader(GameObject lead)
     {
         leader = true;
-        for (int i = 0; i < unitNum; i++)
+        if (!engaged)
         {
-            GameObject pos = new GameObject();
-            pos.transform.parent = transform;
-            pos.transform.localPosition = Vector3.zero;
-            pos.transform.Translate(new Vector3(-i - 0.5f, 0.0f, -(Mathf.Pow(-1.0f, i)) - 0.5f));
-            pos.name = "Tracking Location: " + (i + 1);
+            for (int i = 0; i < (unitCount - 1); i++)
+            {
+                GameObject pos = new GameObject();
+                pos.transform.parent = transform;
+                pos.transform.localPosition = Vector3.zero;
+                pos.transform.Translate(new Vector3(-i - 0.5f, 0.0f, -(Mathf.Pow(-1.0f, i)) - 0.5f));
+                pos.name = "Tracking Location: " + (i + 1);
+            }
+        }
+        engaged = true;
+        foreach (GameObject unit in GameObject.FindGameObjectsWithTag("Unit"))
+        {
+            if (unit && unit != lead)
+            {
+                Unit _unit = unit.GetComponent<Unit>();
+                if (_unit)
+                {
+                    if (_unit.GetTeam() == team)
+                    {
+                        if (_unit.leader)
+                        {
+                            _unit.leader = false;
+                        }
+                        _unit.leadUnit = lead;
+                        _unit.engaged = true;
+                        _unit.StopAllCoroutines();
+                    }
+                }
+            }
+        }
+    }
+
+    private void RemoveLeader(GameObject lead)
+    {
+        leader = false;
+        engaged = false;
+        moving = false;
+        for (int i = 0; i < (unitCount - 1); ++i)
+        {
+            if(transform.GetChild(i).gameObject)
+                Destroy(transform.GetChild(i).gameObject);
+        }
+        foreach (GameObject unit in GameObject.FindGameObjectsWithTag("Unit"))
+        {
+            if (unit && unit != lead)
+            {
+                Unit _unit = unit.GetComponent<Unit>();
+                if (_unit)
+                {
+                    if (_unit.GetTeam() == team)
+                    {
+                        _unit.leadUnit = null;
+                        _unit.engaged = false;
+                        _unit.moving = false;
+                    }
+                }
+            }
         }
     }
 
@@ -354,7 +472,7 @@ public class Unit : MonoBehaviour
 
     private IEnumerator Wander()
     {
-        while (leader)
+        while (!currentTarget)
         {
             Vector3 newPos = RandomNavSphere(transform.position, radius, -1);
             SetDestination(newPos);
@@ -383,26 +501,5 @@ public class Unit : MonoBehaviour
         {
             agent.destination = destination;
         }
-    }
-
-    void OnDrawGizmos()
-    {
-        // Green
-        Gizmos.color = new Color(0.0f, 1.0f, 0.0f);
-        if(forwardLine.neutralArea != null)
-            DrawRect(forwardLine.neutralArea);
-    }
-
-    void OnDrawGizmosSelected()
-    {
-        // Orange
-        Gizmos.color = new Color(1.0f, 0.5f, 0.0f);
-        if(forwardLine.neutralArea != null)
-            DrawRect(forwardLine.neutralArea);
-    }
-
-    void DrawRect(Rect rect)
-    {
-        Gizmos.DrawWireCube(new Vector3(rect.center.x, 0.01f, rect.center.y), new Vector3(rect.size.x, 0.01f, rect.size.y));
     }
 }
